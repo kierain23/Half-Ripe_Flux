f8fa64991cd5c9b9cbfc961222beca62c168d6a9
condomj <conor.jordan7@live.ie>
2014-12-12 17:11:44 +0000

    Added Blood cells and animation

---
+++ Work in progress/Assets/RedBloodCell.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedBloodCell : MonoBehaviour {
+
+	float moveSpeed;
+	public float minSpeed = 1f, maxSpeed = 3f;
+	
+	void Start()
+	{
+		moveSpeed = Random.Range (minSpeed, maxSpeed);
+		
+		float randomY = Random.Range (-1f, 2f);
+		rigidbody2D.AddForce(new Vector2(-moveSpeed, randomY));
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		rigidbody2D.velocity = rigidbody2D.velocity.normalized * moveSpeed;
+		
+		if(this.transform.position.x < -9f)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+}
---
+++ Work in progress/Assets/RedBloodCellSpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedBloodCellSpawner : MonoBehaviour {
+
+    float spawnTimer;
+	float spawnTime = 1f;
+	public float minTime = 1f, maxTime = 4f;
+	public GameObject RedBloodCell;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		spawnTimer += Time.deltaTime;
+		if(spawnTimer >= spawnTime)
+		{
+			spawnTimer = 0;
+			spawnTime = (int)Random.Range (minTime, maxTime);
+			SpawnRedBloodCell();
+		}
+	}
+	
+	void SpawnRedBloodCell()
+	{
+		Debug.Log ("SPAWNED!");
+		GameObject RedBloodCellCopy;
+		float randomY = this.transform.position.y + Random.Range (-3f, 3f);
+		Vector3 randomPosition = new Vector3(this.transform.position.x, randomY, 0); 
+		RedBloodCellCopy = (GameObject)Instantiate (RedBloodCell, randomPosition, this.transform.rotation);
+		
+	}
+}
